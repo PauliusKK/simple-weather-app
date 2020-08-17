b08dd030954b594821ae50e9a9afc323c56231b3
PauliusKK <17347293+PauliusKK@users.noreply.github.com>
2020-08-17 19:29:50 +0300

    Add weatherWidget

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import WeatherWidget from '@eggtronic/react-weather-widget';
 import './App.scss';
 
 function App() {
+  const openWeatherMapApiKey = '6d0a77a68272df5d0b4f222227ef075f';
+
   return (
     <div className="section">
       <div className="container">
         <input type="text" name="city" placeholder="City..." />
 
-
+        <WeatherWidget apiKey={openWeatherMapApiKey} />
       </div>
     </div>
   );
