2964639eea041f5a9d6e9ecf93c9c7496032ec0e
PauliusKK <17347293+PauliusKK@users.noreply.github.com>
2020-08-18 03:20:38 +0300

    Added functionality to get user current location

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import { find } from 'lodash';
 import Cities from './city.list.json';
 import './App.scss';
 
-// TODO:
-// get user location.
-
 function App() {
   const [city, setCity] = useState('');
+  const [cityName, setCityName] = useState('');
   const [cityError, setCityError] = useState(undefined);
   const [forecastList, setForecastList] = useState(undefined);
 
@@ -17,8 +15,51 @@ function App() {
   const weatherDataUnits = 'metric';
 
   const timeToWaitToSearch = 750;
+  let lat, lon;
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+    lat = crd.latitude;
+    lon = crd.longitude;
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
+  function handleWeatherData(url) {
+    return axios.get(url)
+      .then(function (weather) {
+        setCityError(undefined);
+        setCityName(weather.data.city.name);
+        const weatherForecastList = weather.data.list.map((forecast) => {
+          const { dt, main, weather, wind } = forecast;
+          const { temp: temperature, feels_like: feelsLike, humidity } = main;
+          const { main: weatherTitle, description: weatherDescription } = weather[0];
+          const date = moment.unix(dt).utcOffset('+0200').format('YYYY-MM-DD HH:mm');
+          return { date, temperature, feelsLike, humidity, weatherTitle, weatherDescription, wind };
+        });
+
+        setForecastList(weatherForecastList);
+      })
+      .catch(function (error) {
+        setCityError("Oops! Weather could not be loaded. Contact support.");
+      })
+  }
+
   useEffect(() => {
     if (!city) {
+      if (lat && lon) {
+        handleWeatherData(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${weatherDataUnits}&appid=${openWeatherMapApiKey}`)
+      }
       return;
     }
 
@@ -27,25 +68,7 @@ function App() {
 
       if (foundCity) {
         setCityError("City found! Loading weather...");
-
-        axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${foundCity.name}&units=${weatherDataUnits}&appid=${openWeatherMapApiKey}`)
-          .then(function (weather) {
-            setCityError(undefined);
-
-            const weatherForecastList = weather.data.list.map((forecast) => {
-              const { dt, main, weather, wind } = forecast;
-              const { temp: temperature, feels_like: feelsLike, humidity } = main;
-              const { main: weatherTitle, description: weatherDescription } = weather[0];
-              const date = moment.unix(dt).utcOffset('+0200').format('YYYY-MM-DD HH:mm');
-              return { date, temperature, feelsLike, humidity, weatherTitle, weatherDescription };
-            });
-
-            setForecastList(weatherForecastList);
-          })
-          .catch(function (error) {
-            console.log(error);
-            setCityError("Oops! Weather could not be loaded. Contact support.");
-          })
+        handleWeatherData(`http://api.openweathermap.org/data/2.5/forecast?q=${foundCity.name}&units=${weatherDataUnits}&appid=${openWeatherMapApiKey}`)
 
       } else {
         setCityError("Unfortunately, city you've searched for is not found.");
@@ -74,10 +97,10 @@ function App() {
         <div className="forecasts">
           {forecastList && (
             <div>
-              <h2>Forecast for 5 days (every 3 hours)</h2>
+              <h2>Forecast for 5 days (every 3 hours) in {cityName}</h2>
 
               {forecastList.map((forecast) => {
-                const { feelsLike, humidity, temperature, date, weatherDescription, weatherTitle } = forecast;
+                const { feelsLike, humidity, temperature, date, weatherDescription, weatherTitle, wind } = forecast;
 
                 return (
                   <div className="forecast">
@@ -88,6 +111,7 @@ function App() {
                     <h4>Weather:</h4>
                     <p>Title: <strong>{weatherTitle}</strong></p>
                     <p>Description: <strong>{weatherDescription}</strong></p>
+                    <p>Wind speed: <strong>{wind.speed} m/s</strong></p>
                   </div>
                 )
               })}
